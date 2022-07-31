5f3a1734181f99c6591a41d6c92f2b6e2c20f569
stillLucy <101994919+stillLucy@users.noreply.github.com>
2022-07-31 13:16:18 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,118 +1,39 @@
-// eslint-disable-next-line no-unused-expressions
-import.meta.env.API_KEY;
+document.querySelector('#app').innerHTML = `
+  <h1>Movie Locator</h1>
+  <a href="https://vitejs.dev/guide/features.html" target="_blank"></a>
+`;
+const search = (ev) => {
+  ev.preventDefault();
+  const title = document.querySelector("#title").value;
+  const year = document.querySelector("#year").value;
+  const imdb= document.querySelector("#imdb").value;
+  const url = `https://www.omdbapi.com/?t=${title}&y=${year}&imdb=${imdb}&apikey=5ca0070c`;
 
-async function getData(inputVal, titleLen) {
-  const resp = await fetch(
+  console.log(url);
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+      const parent = document.querySelector("#movie-details");
+      while (parent.firstChild) {
+        parent.removeChild(parent.firstChild);
+      }
+      const movieTemplate = `
+    
+  <h2>${data.Title}</h2>
+  <p>${data.Year}</p>
+  <img src="${data.Poster}" alt= "poster image"/>
+  <p>${data.imdb}</p>
+  
+  `;
+      document
+        .querySelector("#movie-details")
+        .insertAdjacentHTML("beforeend", movieTemplate);
+      
 
-    // eslint-disable-next-line no-undef
-    'http://www.omdbapi.com/?i=tt3896198&apikey=5ca0070c',
-  );
+      console.log(data.Title);
+      console.log(data.Poster);
+    });
+};
 
-  const data = await resp.json();
-
-  // eslint-disable-next-line no-console
-  console.log(data);
-  const searchRes = data.Search;
-
-  // eslint-disable-next-line no-console
-  console.log(searchRes);
-
-  // eslint-disable-next-line no-console
-  console.log(`this is your title: ${titleLen}`);
-  // eslint-disable-next-line no-plusplus
-  for (let i = 0; i < searchRes.length; i++) {
-    // eslint-disable-next-line no-console
-    console.log(`this is data: ${searchRes[i].Title}`);
-
-    // eslint-disable-next-line no-undef, vars-on-top, no-var, block-scoped-var
-    var url = `https://omdbapi.com/?s=${searchTerm}&page=1&apikey=VITE_API_KEY`;
-
-    // eslint-disable-next-line no-console, block-scoped-var
-    console.log(`this is your URL ${url}`);
-    // eslint-disable-next-line block-scoped-var
-    const titleResp = fetch(`${url}`);
-    const titleData = titleResp.json();
-
-    // eslint-disable-next-line no-console
-    console.log(titleData.title);
-
-    // eslint-disable-next-line no-console
-    console.log(titleData.Ratings);
-
-    const ratings = titleData.Ratings;
-
-    // eslint-disable-next-line no-shadow, no-plusplus
-    for (let i = 0; i < ratings.length; i++) {
-      // eslint-disable-next-line no-console
-      console.log(`${ratings[i].Source} : ${ratings[i].Value}`);
-      const maincon2 = document.getElementById('main_con');
-      const rating = document.createElement('p');
-      rating.textContent = `${ratings[i].Source} : ${ratings[i].Value}`;
-      maincon2.appendChild(rating);
-    }
-
-    // eslint-disable-next-line global-require
-    require('dotenv').config();
-    // eslint-disable-next-line no-console
-    console.log(process.env);
-
-    // eslint-disable-next-line vars-on-top, no-var
-    var input;
-
-    // eslint-disable-next-line vars-on-top, no-var, no-redeclare
-    var url = 'api + movie + apiKey';
-    // eslint-disable-next-line no-undef
-    const button = select('#submit');
-    // eslint-disable-next-line no-undef
-    button.mousePressed(titleAsk);
-
-    // eslint-disable-next-line no-unused-vars, no-undef
-    input = select('#movie');
-  }
-
-  // console.log(Search.Title)
-  const mainCon = document.getElementById('main_con');
-  // console.log(data.Search[i])
-
-  const imgCont = document.createElement('div');
-  const imgTag = document.createElement('img');
-  const movieID = document.createElement('p');
-  const title = document.createElement('p');
-  const year = document.createElement('p');
-
-  // eslint-disable-next-line no-undef
-  imgTag.src = searchRes[i].Poster;
-  imgTag.setAttribute('style', 'width: 500px');
-
-  // eslint-disable-next-line no-undef
-  title.textContent = searchRes[i].Title;
-  // eslint-disable-next-line no-undef
-  movieID.textContent = searchRes[i].imdbID;
-  // eslint-disable-next-line no-undef
-  year.textContent = searchRes[i].Year;
-  // eslint-disable-next-line no-undef
-  title.textContent = titleData.title;
-
-  imgCont.appendChild(imgTag);
-
-  mainCon.appendChild(title);
-  mainCon.appendChild(movieID);
-  mainCon.appendChild(year);
-  mainCon.appendChild(imgCont);
-}
-
-const button = document.getElementById('button');
-button.addEventListener('click', (e) => {
-  e.preventDefault();
-  const inputVal = document.getElementById('movie_search').value;
-
-  const title = document.querySelector(
-    'input[type=radio][name=title_length]:checked',
-  );
-  const titleLen = title.value;
-
-  // eslint-disable-next-line no-console
-  console.log(`this is title length 3: ${titleLen}`);
-  getData(inputVal, titleLen);
-  // getData(titleLen);
-});
+document.querySelector("form").addEventListener("submit", search);
